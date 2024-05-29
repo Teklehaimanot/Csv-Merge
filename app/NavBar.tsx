425d6dd7eeb2234442648698bd8f8fd07cbadd61
teklehaimanot <teklehaimanotbelete@gmail.com>
2024-05-29 11:20:26 +0300

    initial commit

---
+++ app/NavBar.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <div>
+      <ul>
+        <li>logo</li>
+        <li>one</li>
+        <li>two</li>
+      </ul>
+    </div>
+  );
+};
+
+export default NavBar;
